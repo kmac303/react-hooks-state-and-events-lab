49691a5bdba6ddaf745c24abd32588774284ac5c
support <support@kmcintosh-6782.remax.local>
2022-10-25 18:21:51 -0600

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
 
+
+function App() {
+  // const [items, setItems] = useState(itemData);
+    const [isOn, setIsOn] = useState(false);
+    const [category, setCategory] = useState("All");  
+    function handleClick() {
+      setIsOn((isOn) => !isOn);
+    }
+    
+    console.log(category);
+    const foodsToDisplay = itemData.filter((item) => {
+      if (category === "All") {
+        return true;
+      } else {
+        return item.category === category;
+      }
+    });
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isOn ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>{isOn ? "Light Mode" : "Dark Mode"}</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={foodsToDisplay} setCategory={setCategory}/>
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isOn, setIsOn] = useState(false);
+    function handleClick() {
+      setIsOn((isOn) => !isOn);
+    }
+    const appClass = isOn ? "in-cart" : ""
   return (
-    <li className="">
+    <li className={appClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{isOn ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
